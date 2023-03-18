8071662d3e3cdf105030d0d2d86b7f4fb1698bcb
joshstubble <62226937+joshstubble@users.noreply.github.com>
2023-03-18 18:17:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import contextlib
 from collections import defaultdict
 from aiohttp import ClientSession
 from discord.ext import commands
-from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 from interactions import InteractionClient
 import tweepy
 from sklearn.feature_extraction.text import TfidfVectorizer
